1edf5675792781ace33b80bc4ae736c7038839f2
Eduardo Pessine <eduardopessine@gmail.com>
2024-03-09 20:22:49 -0300

    feat: create error pages

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,6 @@
+<x-layouts.app>
+    <div class="flex h-full w-full flex-col items-center justify-center gap-4">
+        <h1 class="text-5xl font-bold">Ops!</h1>
+        <h2 class="font-mono text-lg">Não encontramos essa página.</h2>
+    </div>
+</x-layouts.app>
---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,6 @@
+<x-layouts.app>
+    <div class="flex h-full w-full flex-col items-center justify-center gap-4">
+        <h1 class="text-5xl font-bold">Ops!</h1>
+        <h2 class="font-mono text-lg">Ocorreu um erro inesperado.</h2>
+    </div>
+</x-layouts.app>
